bbd503b2c46c83a70412dc80302bd69e17398682
Vrezh <vrezhikmarkaryan@gmail.com>
2022-08-28 19:10:31 +0500

    Example005

---
+++ Example/Example005_ConditionifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же МАША!");
+}
+else 
+{
+    Console.Write("Првет, ");
+    Console.WriteLine(username);
+}
